90c0674bb055842179b2db262ff2c88549d47e81
Dudinhahs03 <144931581+Dudinhahs03@users.noreply.github.com>
2023-11-29 08:46:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-document.querySelector('.tecla_pom');
-function TocaSomPom(){}
-document.querySelector('#som_tecla_pom').play('')
-document.querySelector('.tecla_pom').onclick=tocaSomPom();
+function tocasompom(){
+    document.queryselector('#som_tecla_pom').play('');
+}
+document.querySelector('.tecla_pom'); 
+ const listadeteclas= document.querySelectorAll('.tecla');
+listadeteclas[0].onclick= tocasompom;
+
+
+for(contador = 0; contador< listadeteclas.length;contador ++){
+    const instrumento = listadeteclas[contador].classList[1];
+}
+ const tecla = listadeteclas[contador];
+
+   const idaudio = #som_${instrumento};
